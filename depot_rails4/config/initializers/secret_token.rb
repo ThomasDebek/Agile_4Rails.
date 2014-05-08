b37dab02f53d30176e6702d4d6fece99ae30215c
ThomasDebek <tom.debek@gmail.com>
2014-05-08 11:11:05 +0200

    Api Sklepu - depot_rails4

---
+++ depot_rails4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DepotRails4::Application.config.secret_key_base = '7b77458f93936a8f991146763aeefa356995f99be8680a9ec4a9264e68a2c79a81f3aff828d619d115f47a06409723024c4290934f1952a8a0dd368e509de9ca'
