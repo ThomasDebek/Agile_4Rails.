62411f818dbbd8410fc5e017ac82091f82515f42
ThomasDebek <tom.debek@gmail.com>
2014-05-09 18:47:07 +0200

    Odrobina testów do sklepu, opierają sie na fixturach - tam mam moje dane

--- depot_rails4/test/controllers/store_controller_test.rb
+++ depot_rails4/test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 4  # Ten mi sprwadza czy są te kolumny
+    assert_select '#main .entry', 3               # Ten czy istnieją te elementy
+    assert_select 'h3', 'Programming Ruby 1.9'    # Czy jest to tytol ksiazki
+    assert_select '.price', /\$[,\d]+\.\d\d/      # A tu naszą poprawnosc ceny
   end
 
 end
