9d9213ffe4ec72ec2fee55fa65a5823de8078f8c
ThomasDebek <tom.debek@gmail.com>
2014-05-12 10:39:28 +0200

    Musimy stworzyc inteligentą metode, która sprwdza czy nasza lista elementów ma już w sobie produkty, jesli tak to wpada ilosć a jeśli nie to budujemy nowe polączenie

--- depot_rails4/app/models/cart.rb
+++ depot_rails4/app/models/cart.rb
@@ -1,3 +1,21 @@
 class Cart < ActiveRecord::Base
-  has_many :line_items, dependent: :destroy      # koszyk ma potencjalnie wiele związancyh polaczen,
-end                                              # jeżeli zniszczymy nasz koszyk to chcemy także zeby zniszczyc wszystkie pozycjie z nim związane
+  has_many :line_items, dependent: :destroy # koszyk ma potencjalnie wiele związancyh polaczen,
+  # jeżeli zniszczymy nasz koszyk to chcemy także zeby zniszczyc wszystkie pozycjie z nim związane
+
+
+  def add_product(product_id)                                  # Tutaj tworzymy inteligętną metodę, która sprawdza
+    current_item = line_items.find_by(product_id: product_id)  # czy nasza lista elementów ma juz w sobie produkty
+    if current_item                                            # jesli tak to wpada ilosc
+      current_item.quantity += 1                               # a jesli nie to budujemy nowy polonczenie line_items
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+end
+
+
+
+
+
+
