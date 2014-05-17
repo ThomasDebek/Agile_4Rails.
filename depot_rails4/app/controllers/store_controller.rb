938040fa37cd0935962ab38c5befc4adfa075681
ThomasDebek <tom.debek@gmail.com>
2014-05-17 21:42:02 +0200

    Will_paginate w sklepie

--- depot_rails4/app/controllers/store_controller.rb
+++ depot_rails4/app/controllers/store_controller.rb
@@ -7,7 +7,10 @@ class StoreController < ApplicationController
     if params[:set_locale]
       redirect_to store_url(locale: params[:set_locale])
     else
-      @products = Product.order(:title)
+
+
+      @products = Product.all.paginate(page: params[:page], per_page: 3)
+
     end
   end
 end
